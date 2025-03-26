d1b8eb31883041d97a7c4c915ee89652b0c61d6c
grigor <musoyangrigor251@gmail.com>
2025-03-26 13:21:45 +0400

    Added assignment variable logic to interpreter, enabling variable referencing

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stack>
 #include <cmath>
+#include <algorithm>
 
 Interpreter::Interpreter() = default;
 
@@ -25,34 +26,29 @@ void Interpreter::interpretLine(const std::string &line) {
     std::string command;
     stream >> command;
 
-    const std::size_t size = command.size();
+    if (!line.empty() && line.find('=') != std::string::npos) {
+        size_t equal_pos = line.find('=');
 
-    if (command.substr(0, 6) == "print(" && command[size - 1] == ')' && size > 6) {
+        std::string left_side = line.substr(0, equal_pos);
+        std::string expression = line.substr(equal_pos + 1);
+
+        left_side.erase(left_side.find_last_not_of(" \t") + 1);
+        expression.erase(0, expression.find_first_not_of(" \t"));
+
+        handleVariableAssignment(left_side, expression);
+    }
+
+    if (const std::size_t size = command.size();
+        command.substr(0, 6) == "print(" && command[size - 1] == ')' && size > 6) {
+        // print() command
         std::string args = command.substr(6, size - 7);
         std::getline(stream, args);
         handlePrintCommand(args);
     } else if (command.substr(0, 8) == "println(" && command[size - 1] == ')' && size > 8) {
+        // println() command
         std::string args = command.substr(8, size - 9);
         std::getline(stream, args);
         handlePrintlnCommand(args);
-    } else if (std::isalpha(command[0]) && size > 0) {
-        std::string rest;
-        std::getline(stream, rest);
-        const size_t equalPos = rest.find('=');
-
-        if (equalPos != std::string::npos) {
-            std::string varName = command;
-            std::string value = rest.substr(equalPos + 1);
-
-            varName.erase(varName.find_last_not_of(" \t") + 1);
-            value.erase(0, value.find_first_not_of(" \t"));
-
-            handleVariableAssignment(varName, value);
-        } else {
-            std::cerr << "Syntax error: " << command << std::endl;
-        }
-    } else {
-        std::cerr << "Syntax error: " << command << std::endl;
     }
 }
 
@@ -85,20 +81,31 @@ void Interpreter::handlePrintlnCommand(const std::string &args) {
 
 void Interpreter::handleVariableAssignment(const std::string &name, const std::string &value) {
     std::string trimmedValue = value;
-    if (trimmedValue.front() == '"' && trimmedValue.back() == '"') {
+
+    if (variables.contains(trimmedValue)) {
+        trimmedValue = variables[trimmedValue];
+    }
+    else if (trimmedValue.find_first_of("+-*/^") != std::string::npos) {
+        try {
+            trimmedValue = std::to_string(eval(trimmedValue));
+        } catch (...) {}
+    }
+    else if (trimmedValue.front() == '"' && trimmedValue.back() == '"') {
         trimmedValue = trimmedValue.substr(1, trimmedValue.size() - 2);
-    } else {
-        trimmedValue = std::to_string(eval(trimmedValue));
     }
 
     variables[name] = trimmedValue;
 }
 
 std::string Interpreter::evaluateExpression(const std::string &expr) {
-    if (variables.contains(expr)) {
+    if (variables.contains(expr) > 0) {
         return variables[expr];
     }
-    return expr;
+    try {
+        return std::to_string(eval(expr));
+    } catch (...) {
+        return expr;
+    }
 }
 
 int Interpreter::precedence(const char op) {
@@ -121,20 +128,21 @@ double Interpreter::applyOp(const double num1, const double num2, const char op)
             if (num2 == 0) {
                 throw std::runtime_error("Error: Division by zero");
             }
-        return num1 / num2;
+            return num1 / num2;
         case '^': return pow(num1, num2);
         default:
             throw std::invalid_argument("Invalid operator");
     }
 }
 
-
 double Interpreter::eval(const std::string &expression) {
     std::stack<double> values;
     std::stack<char> ops;
 
     for (size_t i = 0; i < expression.length(); i++) {
-        if (isspace(expression[i])) continue;
+        if (isspace(expression[i])) {
+            continue;
+        }
 
         if (isdigit(expression[i])) {
             double val = 0;
